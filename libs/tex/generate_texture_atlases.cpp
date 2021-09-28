49ab6f60cb179d6b2bbea97c78d0d414ff0e06d5
vivekmistry <viv.mistry@gmail.com>
2021-09-28 22:08:31 +0200

    Update generate_texture_atlases.cpp

--- libs/tex/generate_texture_atlases.cpp
+++ libs/tex/generate_texture_atlases.cpp
@@ -123,6 +123,7 @@ generate_texture_atlases(std::vector<TexturePatch::Ptr> * orig_texture_patches,
 
     while (!texture_patches.empty()) {
         unsigned int texture_size = calculate_texture_size(texture_patches);
+        std::cout << texture_size;
 
         texture_atlases->push_back(TextureAtlas::create(texture_size));
         TextureAtlas::Ptr texture_atlas = texture_atlases->back();

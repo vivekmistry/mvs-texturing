645513fd09716f0b14b7e7f488c79abffdab55a1
vivekmistry <viv.mistry@gmail.com>
2021-09-28 09:55:04 +0200

    Update generate_texture_atlases.cpp

--- libs/tex/generate_texture_atlases.cpp
+++ libs/tex/generate_texture_atlases.cpp
@@ -21,7 +21,7 @@
 #include "texture_patch.h"
 #include "texture_atlas.h"
 
-#define MAX_TEXTURE_SIZE (8 * 1024)
+#define MAX_TEXTURE_SIZE (150 * 1024)
 #define PREF_TEXTURE_SIZE (4 * 1024)
 #define MIN_TEXTURE_SIZE (256)
 
